63e5da370ab14e595a5f84d9913ff547d5fed760
ivonzhang-kingdee <zhangivon@gmail.com>
2017-04-26 14:31:24 +0800

    Koa1.0版本下 项目的入口文件 定义了node服务 从这里启动

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const db = 'mongodb://localhost/test'
+
+/**
+ * mongoose连接数据库
+ * @type {[type]}
+ */
+mongoose.Promise = require('bluebird')
+mongoose.connect(db)
+
+/**
+ * 获取数据库表对应的js对象所在的路径
+ * @type {[type]}
+ */
+const models_path = path.join(__dirname, '/app/models')
+
+
+/**
+ * 已递归的形式，读取models文件夹下的js模型文件，并require
+ * @param  {[type]} modelPath [description]
+ * @return {[type]}           [description]
+ */
+var walk = function(modelPath) {
+  fs
+    .readdirSync(modelPath)
+    .forEach(function(file) {
+      var filePath = path.join(modelPath, '/' + file)
+      var stat = fs.statSync(filePath)
+
+      if (stat.isFile()) {
+        if (/(.*)\.(js|coffee)/.test(file)) {
+          require(filePath)
+        }
+      }
+      else if (stat.isDirectory()) {
+        walk(filePath)
+      }
+    })
+}
+walk(models_path)
+
+
+var koa = require('koa')
+var bodyParser = require('koa-bodyparser')
+var logger = require('koa-logger')
+var session = require('koa-session')
+// var koaBody = require('koa-body')
+var app = koa()
+app.use(bodyParser())
+app.use(logger())
+app.use(session(app))
+
+
+
+/**
+ * 使用路由转发请求
+ * @type {[type]}
+ */
+var router = require('./config/router')()
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(1234)
+console.log('app started at port 1234...');
